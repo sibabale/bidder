b43a5314dcbfe12e66ef7d6a59e6460c4e053c9e
sibabale <sibabalejoja@gmail.com>
2024-10-19 12:01:26 +0200

    refactor: checking the expiry date on our tokens and block request with expired tokens

--- poc/src/middleware/auth/verifyToken.js
+++ poc/src/middleware/auth/verifyToken.js
@@ -8,11 +8,18 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+
+    // Check if the token is expired
+    if (decodedToken.exp < currentTime) {
+      return res.status(401).json({ message: 'Token has expired' });
+    }
+
     req.userId = decodedToken.uid; // Attach user ID to request
     next(); // Proceed to next middleware/handler
   } catch (error) {
     console.error('Error verifying token:', error);
-    res.status(401).json({ message: 'Invalid or expired token' });
+    res.status(401).json({ message: 'Invalid token' });
   }
 };
 
