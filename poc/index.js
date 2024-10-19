6a9d95719a3fc948efa30257962b519d5fedab1d
sibabale <sibabalejoja@gmail.com>
2024-10-19 10:46:46 +0200

    refactor: update the the custom claims route

--- poc/index.js
+++ poc/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   app.use('/api/register', register);
 
   // Use the setClaims router
-  app.use('/api/setCustomClaims', setCustomClaims);
+  app.use('/api/set-custom-claims', setCustomClaims);
   
 
 // WebSocket connection handling
