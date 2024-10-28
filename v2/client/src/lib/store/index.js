c5d533b24d162a18a4d14661ef7019d9e26a73d1
sibabale <sibabalejoja@gmail.com>
2024-10-28 17:59:46 +0200

    refactor: updated my store

--- v2/client/src/lib/store/index.js
+++ v2/client/src/lib/store/index.js
@@ -1,31 +1,66 @@
-import storage from 'redux-persist/lib/storage'
+// 'use client'
+// import storage from 'redux-persist/lib/storage'
+// import { persistStore, persistReducer } from 'redux-persist'
+// import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+// import userReducer from './slices/user/index.js'
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['user', 'cart', 'orders'],
+// }
+
+// const rootReducer = combineReducers({
+//     user: userReducer,
+// })
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = () => {
+//     return configureStore({
+//         reducer: persistedReducer,
+//         middleware: (getDefaultMiddleware) =>
+//             getDefaultMiddleware({
+//                 serializableCheck: false,
+//             }),
+//     })
+// }
+
+// const persistor = persistStore(store())
+
+// export { store, persistor }
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from 'redux-persist'
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './slices/user/index.js'
 
 const persistConfig = {
-    key: 'root',
+    key: 'persist',
     storage,
-    whitelist: ['user', 'cart', 'orders'],
 }
 
 const rootReducer = combineReducers({
     user: userReducer,
 })
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-const store = () => {
-    return configureStore({
-        reducer: persistedReducer,
-        middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware({
-                serializableCheck: false,
-            }),
+const makeConfiguredStore = () =>
+    configureStore({
+        reducer: rootReducer,
     })
+
+export const store = () => {
+    const isServer = typeof window === 'undefined'
+    if (isServer) {
+        return makeConfiguredStore()
+    } else {
+        const persistedReducer = persistReducer(persistConfig, rootReducer)
+        let store = configureStore({
+            reducer: persistedReducer,
+        })
+        store.__persistor = persistStore(store)
+        return store
+    }
 }
-
-const persistor = persistStore(store())
-
-export { store, persistor }
