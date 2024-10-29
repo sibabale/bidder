b02b7348e174f294579d9a1b7e3f2ad3eea23ec6
sibabale <sibabalejoja@gmail.com>
2024-10-29 19:58:14 +0200

    refactor: reading service key from the env

--- server/src/config/firebase-admin.js
+++ server/src/config/firebase-admin.js
@@ -1,7 +1,19 @@
 
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../serviceAccountKey.json');
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    auth_uri: process.env.FIREBASE_TYPE,
+    client_id: process.env.FIREBASE_TYPE,
+    token_uri: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_TYPE,
+    private_key: process.env.FIREBASE_TYPE,
+    client_email: process.env.FIREBASE_TYPE,
+    private_key_id: process.env.FIREBASE_TYPE,
+    universe_domain: process.env.FIREBASE_TYPE,
+    client_x509_cert_url: process.env.FIREBASE_TYPE,
+    auth_provider_x509_cert_url: process.env.FIREBASE_TYPE,
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
