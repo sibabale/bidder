0e22f22e06b32a69626dd53c5cfc3befe8cf748a
sibabale <sibabalejoja@gmail.com>
2024-10-29 20:07:50 +0200

    UPDATE

--- server/src/config/firebase-admin.js
+++ server/src/config/firebase-admin.js
@@ -2,17 +2,17 @@
 const admin = require('firebase-admin');
 
 const serviceAccount = {
-    "type": process.env.FIREBASE_TYPE,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "type": process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+    "auth_uri": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+    "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+    "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+    "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "universe_domain": process.env.FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+    "client_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
 };
 
 admin.initializeApp({
