b9aa5632ec49c7ce59f727ef249c9b7c38ef38de
sibabale <sibabalejoja@gmail.com>
2024-10-29 19:28:10 +0200

    refactor: allowing options

--- server/src/middleware/cors.js
+++ server/src/middleware/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: process.env.CORS_WHITELIST.split(','),
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200,
