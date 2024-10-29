dcc7e2721a9c341c0f44a8e59c203f07f223a147
sibabale <sibabalejoja@gmail.com>
2024-10-29 20:15:50 +0200

    formatting private key

--- server/src/config/firebase-admin.js
+++ server/src/config/firebase-admin.js
@@ -7,7 +7,7 @@ const serviceAccount = {
     "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
     "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
     "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
-    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
     "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
     "universe_domain": process.env.FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
--- server/src/middleware/cors.js
+++ server/src/middleware/cors.js
@@ -1,10 +1,6 @@
 const cors = require('cors');
 
 
-console.log(process.env.CORS_WHITELIST);
-console.log(process.env.CORS_WHITELIST.split(','));
-
-
 const corsOptions = {
   origin: process.env.CORS_WHITELIST.split(','),
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
