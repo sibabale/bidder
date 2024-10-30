431474bcc07bf4963896fa19f2ad5ad3c8029f1a
sibabale <sibabalejoja@gmail.com>
2024-10-30 17:57:39 +0200

    formatting

--- server/src/config/firebase-admin.js
+++ server/src/config/firebase-admin.js
@@ -8,7 +8,6 @@ const serviceAccount = {
     "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
     "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
     "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
-    // "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "private_key": private_key,
     "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
     "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
