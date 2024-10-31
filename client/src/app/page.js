c1d214d2b2cb8995f92cb13dfd07ef8523cbba9c
sibabale <sibabalejoja@gmail.com>
2024-10-31 11:02:30 +0200

    responsive home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,12 +2,12 @@ export default function Home() {
     return (
         <>
             {/* <Header /> */}
-            <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center">
+            <div className="p-3 min-h-screen bg-gray-50 flex flex-col items-center justify-center">
                 <h1 className="text-3xl font-bold">Welcome to Biddar</h1>
                 <p className="mt-4 text-gray-700">
                     Join live auctions and place your bids in real-time.
                 </p>
-                <div className="mt-6 space-x-4">
+                <div className="mt-6 space-x-4 flex flex-col md:flex-row items-center justify-center">
                     <a
                         href="/auth/login"
                         className="px-4 py-2 bg-bidder-primary text-white"
@@ -16,7 +16,7 @@ export default function Home() {
                     </a>
                     <a
                         href="/auth/login"
-                        className="px-4 py-2 border border-bidder-bg-bidder-primary text-bidder-bg-bidder-primary"
+                        className="px-4 mt-5 md:mt-0 py-2 border border-bidder-bg-bidder-primary text-bidder-bg-bidder-primary"
                     >
                         Login
                     </a>
