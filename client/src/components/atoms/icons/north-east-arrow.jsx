e7311447aa78b0770197089d557f980006fac46a
sibabale <sibabalejoja@gmail.com>
2024-11-02 14:28:40 +0200

    new icon

---
+++ client/src/components/atoms/icons/north-east-arrow.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const NorthEastArrowIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="#ff4500"
+        viewBox="0 -960 960 960"
+    >
+        <path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264z"></path>
+    </svg>
+)
+
+export default NorthEastArrowIcon
